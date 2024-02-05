43dfe30a37777d9d802522af256414533eb11af1
Vinicius Correia Souza <154944076+CorreiaSouza@users.noreply.github.com>
2024-02-05 19:18:17 -0300

    steve

--- projetosideal/menu.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = document.getElementById("btn-mobile");
-const menuDesktop = document.querySelector(".menu.desktop");
-
-function toggleMenu() {
-    menuDesktop.classList.toggle('active');
-}
-
-btnMobile.addEventListener('click', toggleMenu);
