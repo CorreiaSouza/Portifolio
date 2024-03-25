4667579cc2a6183306c3d1fe419ac9f7765c038c
Vinicius Correia Souza <154944076+CorreiaSouza@users.noreply.github.com>
2024-03-24 21:59:50 -0300

    uppdate

--- projetosideal/menu.js
+++ projetosideal/menu.js
@@ -1,16 +1,1 @@
-const menuDiv = document.getElementById('menu-mobile');
-const btnAnimar = document.getElementById('btn-menu');
-
-btnAnimar.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    menuDiv.classList.toggle('abrir');
-    btnAnimar.classList.toggle('ativar');
-}
-
-
-function animarMenu(){
-    const btn = document.getElementById('btn-menu');
-    btn.classList.toggle('ativar');
-}
-
+const menuDiv=document.getElementById("menu-mobile"),btnAnimar=document.getElementById("btn-menu");btnAnimar.addEventListener("click",toggleMenu);function toggleMenu(){menuDiv.classList.toggle("abrir"),btnAnimar.classList.toggle("ativar")}function animarMenu(){const a=document.getElementById("btn-menu");a.classList.toggle("ativar")}
